c9c69e295dd862a6833698f509b574d24d063c4d
ranitoukhy <toukhyr@gmail.com>
2024-04-24 12:21:34 +0300

    24.4 minor changes

--- src/Party.py
+++ src/Party.py
@@ -20,13 +20,13 @@ class StatusCode(enum.Enum):
     BAD_REQUEST = "BAD_REQUEST"
 
 
-class Request:
-    def __init__(self, sender):
+class Message:
+    def __init__(self, sender_id):
         self.is_active = True
-        self.sender = sender
+        self.sender_id = sender_id
 
 
-class PayRequest(Request):
+class PayMessage(Message):
     def __init__(self, token_id, version, owner, sender):
         super().__init__(sender)
         self.token_id = token_id
@@ -34,6 +34,10 @@ class PayRequest(Request):
         self.owner = owner
 
 
+class EchoPayMessage(Message):
+    def __init__(self, sender_id, pay_message: PayMessage):
+        super().__init__(sender_id)
+        self.pay_message = pay_message
 
 
 class Party:
@@ -48,30 +52,25 @@ class Party:
             return StatusCode.OMISSION_TOO_LONG
         pass
 
-    def pay(self, pay_request: PayRequest):
-        if self._handle_omission() == StatusCode.OMISSION_TOO_LONG:
-            return StatusCode.OMISSION_TOO_LONG
-
+    def pay(self, pay_request: PayMessage, party_type: PartyType):
         if pay_request in self.current_requests:
             return None
 
+        if self._handle_omission() == StatusCode.OMISSION_TOO_LONG:
+            return StatusCode.OMISSION_TOO_LONG
+
         self.current_requests.add(pay_request)
 
+        responses = set()
         all_servers = AllEntities().get_servers()
-        if self.party_type == PartyType.CLIENT:
-            for server in all_servers:
-                server.pay(pay_request)
-
-        if self.party_type == PartyType.SERVER:
-            for server in all_servers:
-                if server.id != self.id:
-                    server.pay(pay_request)
+        for server in all_servers:
+            responses.add(server.pay(pay_request, self.party_type))
 
         return StatusCode.OK
 
     def pay(self, token_id, version, owner_id):
-        pay_request = PayRequest(token_id, version, owner_id, self.id)
-        return self.pay(pay_request)
+        pay_request = PayMessage(token_id, version, owner_id, self.id)
+        return self.pay(pay_request, self.party_type)
 
 
     def receive_message(self, sender, message):
